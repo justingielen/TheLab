1f227d66166711f75cd9624be318f8c270fda901
justingielen <justingielen@comcast.net>
2024-05-05 18:19:25 -0400

    white transparent logo (& EventAttendee fields = fk's p3) p2

--- events/models.py
+++ events/models.py
@@ -5,7 +5,9 @@ from page.models import Event
 # This needs to change, User will be an optional field in the Attendee model, to allow parents to sign up their kids without necessarily making User accounts for them
 # Event Attendee will still exist
 class EventAttendee(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50, default="(first name)")
+    first_name = models.CharField(max_length=100, default = "(last name)")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, )
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
